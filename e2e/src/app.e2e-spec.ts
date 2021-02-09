e7326d2ed1f5c195967fad11f797f5b304346b6e
Jesus Enmanuel <edelacruz9713@gmail.com>
2021-02-09 07:28:04 -0400

    First commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('prm-portal app is running!');
+    expect(await page.getTitleText()).toEqual('angular-boilerplate app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/prm-portal'),
+      dir: require('path').join(__dirname, './coverage/angular-boilerplate'),
       subdir: '.',
       reporters: [
         { type: 'html' },
